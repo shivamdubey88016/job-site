862a82308e71d281bb0b7b3a7c421a164e8629ea
shivam dubey <shivamdubey88016@gmail.com>
2025-09-27 13:38:30 +0530

    Use Render dynamic port and serve React frontend

--- backend/index.js
+++ backend/index.js
@@ -33,10 +33,9 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 //  Serve frontend (keep this BEFORE app.listen)
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
